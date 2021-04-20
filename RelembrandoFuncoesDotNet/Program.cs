44dd2327cd93038c2b09363239ce5c506e490ddb
Lucas <lucasgunner3@gmail.com>
2021-04-20 18:20:47 -0300

    Trabalhando com funções estáticas com e sem retorno

--- RelembrandoFuncoesDotNet/Program.cs
+++ RelembrandoFuncoesDotNet/Program.cs
@@ -4,9 +4,22 @@ namespace RelembrandoFuncoesDotNet
 {
     class Program
     {
+
+        public static int SomaNumeros(int a, int b) //Criando função estática para rodar dentro do Console
+        {
+            return a + b;
+        }
+
+        public static void PrintaNome(string nome)
+        {
+            Console.WriteLine(nome);
+        }
+
         static void Main(string[] args) //Quando se utiliza o tipo static você não cria uma instância da sua função
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(SomaNumeros(10,5));
+            PrintaNome("Lucas");
+            Console.Read();
         }
     }
 }
