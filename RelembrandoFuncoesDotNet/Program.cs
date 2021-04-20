187adb4ccebd858e24659860dead9dc6a1f8cc3c
Lucas <lucasgunner3@gmail.com>
2021-04-20 18:10:24 -0300

    Adicionar arquivos de projeto.

---
+++ RelembrandoFuncoesDotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RelembrandoFuncoesDotNet
+{
+    class Program
+    {
+        static void Main(string[] args) //Quando se utiliza o tipo static você não cria uma instância da sua função
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
